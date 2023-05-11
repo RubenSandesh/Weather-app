94bf01067e31b241146c2746831ae44a41f46de5
RubenSandesh <133231514+RubenSandesh@users.noreply.github.com>
2023-05-11 20:01:06 +0530

    weather app

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
+
+const api = {
+  key: "7a7832317dc4b635242ab14f824d2978",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const searchPressed = () => {
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+      });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* HEADER */}
+        <h1>Weather App</h1>
+
+        {/* Search Box*/}
+        <div>
+         <input 
+           type="text" 
+           placeholder="Enter city/town..."
+           onChange={(e)=>  setSearch(e.target.value)} 
+         />
+
+         <button onClick={searchPressed}>Search</button>
+        </div>
+        
+
+        {/* Location */}
+        {typeof weather.main !== "undefined" ? (
+          <div>
+             <p>{weather.name}</p>
+
+             {/* Temperature F/C */}
+             <p>{ weather.main.temp }°F</p>
+
+             {/* Condition (Sunny) */}
+             <p>{weather.weather[0].main }</p>
+             <p>{weather.weather[0].description }</p>
+          </div>
+        ) : (
+          ""
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
